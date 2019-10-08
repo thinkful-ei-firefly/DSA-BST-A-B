099f0f7e01f1268952f2059a1a1c7664dace5112
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-10-08 12:33:09 -0400

    finish 5

--- drills.js
+++ drills.js
@@ -1,12 +1,45 @@
+const BinarySearchTree = require('./BinarySearchTree');
 
-const BinarySearchTree = require('./BinarySearchTree')
-
-function main () {
-    const BST = new BinarySearchTree();
-    const inserts = [3,1,4,6,9,2,5,7]
-    const strings = ['E', 'A', 'S', 'Y', 'Q', 'U', 'E' ,'S', 'T', 'I', 'O' ,'N']
-    strings.forEach ( element =>   BST.insert(element))
-    return BST;
+function main() {
+  const BST = new BinarySearchTree();
+  const inserts = [3, 1, 4, 6, 9, 2, 5, 7];
+  //   const strings = ['E', 'A', 'S', 'Y', 'Q', 'U', 'E', 'S', 'T', 'I', 'O', 'N'];
+  inserts.forEach(element => BST.insert(element));
+  //   strings.forEach(element => BST.insert(element));
+  return BST;
 }
-console.log(main());
+// console.log(main());
 
+/* 
+
+4. What does this program do?
+
+function tree(t){
+    if(!t){
+        return 0;
+    }
+    return tree(t.left) + t.value + tree(t.right)
+}
+
+This function sums all of the values in a tree.
+
+*/
+
+// const sumTree = main();
+// 3, 1, 4, 6, 9, 2, 5, 7 => expect 37
+
+// changed 'value' to 'key' because all of our values were null
+function tree(t) {
+  if (!t) {
+    return 0;
+  }
+  return tree(t.left) + t.key + tree(t.right);
+}
+
+// console.log(sumTree);
+// console.log(`
+
+// `);
+// console.log(tree(sumTree));
+
+////////////////////////////////////////////////////////
